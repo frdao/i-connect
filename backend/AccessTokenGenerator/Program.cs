792a1b063f7978dab42e9b1c0fe4f46716ec2e06
Johnobj <johnole.bjerke@gmail.com>
2022-08-29 12:28:04 +0200

    Create AccessToken generator script

--- backend/AccessTokenGenerator/Program.cs
+++ backend/AccessTokenGenerator/Program.cs
@@ -3,15 +3,33 @@ using Azure;
 using Azure.Core;
 using Azure.Communication.Identity;
 
-namespace AccessTokensQuickstart
+namespace AccessTokenGenerator
 {
     class Program
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Azure Communication Services - Access Tokens Quickstart");
+            Console.WriteLine("Azure Communication Services - Access Tokens Generator");
 
-            // Quickstart code goes here
+            // Gets connectionstring from and environmental variable
+            //TODO: Use Azure Active Directory instead for production as this is temp for testing purposes
+            string? connectionString = Environment.GetEnvironmentVariable("COMMUNICATION_SERVICES_CONNECTION_STRING");
+            if(connectionString is null) throw new NullReferenceException(nameof(connectionString));
+
+            var client = new CommunicationIdentityClient(connectionString);
+
+            // Issue an identity and an access token with the "Chat" scope for the new identity
+            var identityAndTokenResponse = await client.CreateUserAndTokenAsync(scopes: new[] { CommunicationTokenScope.Chat });
+
+            // Retrieve the identity, token, and expiration date from the response
+            var identity = identityAndTokenResponse.Value.User;
+            var token = identityAndTokenResponse.Value.AccessToken.Token;
+            var expiresOn = identityAndTokenResponse.Value.AccessToken.ExpiresOn;
+
+            // Print the details to the screen
+            Console.WriteLine($"\nCreated an identity with ID: {identity.Id}");
+            Console.WriteLine($"\nIssued an access token with 'Chat' scope that expires at {expiresOn}:");
+            Console.WriteLine(token);
         }
     }
 }
