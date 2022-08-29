b3809aeee6ad0e84a28b530759b9ac05d587d46e
Johnobj <johnole.bjerke@gmail.com>
2022-08-29 11:10:03 +0200

    init chatservice and tokengen

---
+++ backend/AccessTokenGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Azure;
+using Azure.Core;
+using Azure.Communication.Identity;
+
+namespace AccessTokensQuickstart
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Communication Services - Access Tokens Quickstart");
+
+            // Quickstart code goes here
+        }
+    }
+}
