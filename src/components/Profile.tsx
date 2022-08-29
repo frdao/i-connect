a95164ab002e9c37bc45d2d51e27c7dfc0cbb383
SIVARANJAN Aschmirthan <a.sivaranjan@soprasteria.com>
2022-08-29 10:49:53 +0200

    #3 reconstuct code and create profile class

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+
+export class Profile {
+    name: string;
+    age: number;
+    imgUrl: string;
+    education: string; 
+    description: string; 
+
+   
+    constructor(name: string ,age: number, imgUrl: string, education: string, description: string) {
+      this.name = name;
+      this.age = age; 
+      this.imgUrl = imgUrl;
+      this.education = education;
+      this.description = description;
+    }
+   
+    /* greet() {
+      return "Hello, " + this.greeting;
+    } */
+  }
+
