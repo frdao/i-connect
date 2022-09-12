a8c75c07b74aeec2441b40ca6af00e1e4cf75115
DAO Francis <francis.dao@soprasteria.com>
2022-09-12 12:47:38 +0200

    #4 chat components

---
+++ src/components/Conversation.tsx
@@ -0,0 +1,10 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Conversation.css';
+
+const Conversation: React.FC = () => {
+  return (
+    <div className='conversationContainer'>This is a conversation</div>
+  );
+};
+
+export default Conversation;
